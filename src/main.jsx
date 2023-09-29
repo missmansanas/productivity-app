a2c26989f5b671714de433ef088f4d2d19f7225b
paolanocom <paolanocom@gmail.com>
2023-09-29 23:17:08 +0800

    Fix: BrowserRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,44 +4,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import {
-  createBrowserRouter,
+  BrowserRouter,
   RouterProvider,
+  Routes,
+  Route,
 } from "react-router-dom";
 import Pomodoro from './components/Pomodoro'
 import TodoList from './components/TodoList'
 import ErrorPage from './components/ErrorPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: "/",
-        element: <TodoList/>,
-      },
-      {
-        path: "pomodoro",
-        element: <Pomodoro/>,
-      },
-    ]
-  },
-],
-{
-  basename: "/productivity-app"
-});
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage/>,
+//     children: [
+//       {
+//         path: "/",
+//         element: <TodoList/>,
+//       },
+//       {
+//         path: "pomodoro",
+//         element: <Pomodoro/>,
+//       },
+//     ]
+//   },
+// ],
+// {
+//   basename: '/productivity-app'
+// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <BrowserRouter basename='/productivity-app'>
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter basename='/productivity-app'>
       <Routes>
         <Route path='/' element={<App/>}>
           <Route path='/' index element={<TodoList/>} />
           <Route path='/pomodoro' element={<Pomodoro/>} />
         </Route>
       </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>,
 )
